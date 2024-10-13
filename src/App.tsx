106fe81e57e7cb25983d9c8ae4f71c7371de6aea
Bence A. Toth <tothab@gmail.com>
2024-10-13 15:45:16 +0200

    Copied files from gamepad controller project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import useGamepad from "./useGamepad";
+
+import "./App.css";
+// import Controller from "./Controller";
+
+const App = () => {
+  // const gamepadKeys = useGamepad();
+
+  return (
+    <div className="app">{/* <Controller gamepadKeys={gamepadKeys} /> */}</div>
+  );
+};
+
+export default App;
